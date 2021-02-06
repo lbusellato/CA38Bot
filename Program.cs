44483274826912669d258931b3b61c11290040fb
lbusellato <67551616+lbusellato@users.noreply.github.com>
2021-02-06 16:09:35 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ca38Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
